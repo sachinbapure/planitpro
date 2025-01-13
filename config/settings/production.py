8e8695476d629a8de1dc0f34bcce325a8cae56e5
sachinbapure <sachinbapure@gmail.com>
2025-01-13 21:57:28 +0530

    lets see

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,13 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["planit.pro"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["planit.pro", ".herokuapp.com"])
+
+# CSRF Settings
+CSRF_TRUSTED_ORIGINS = [
+    "https://planit.pro",
+    "https://*.herokuapp.com",
+]
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -32,7 +38,7 @@ CACHES = {
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
-SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
+SECURE_SSL_REDIRECT = False
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
 SESSION_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
